38ffa3683927d47cd0b7402001ef9f03615dfd48
sakshi <patilsakshi7158@gmail.com>
2025-06-30 15:40:26 +0530

    maven

---
+++ sample.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent {
+        label 'node1'
+    }
+    stages {
+        stage('pull') {
+            steps {
+               git 'https://github.com/SakshiPPatil/studentdata.git'
+                echo "pulling successful"
+            }
+        }
+        stage('building') {
+            steps {
+                sh 'sudo mvn clean package'
+                echo "building successful"
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "testing successful"
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploy successful"
+            }
+        }
+    }
+}
